b2fa12a56893e7635ec7a3023977cb1e8612602f
Joyan Serrao <joyan.s03@gmail.com>
2021-07-13 04:20:15 +0000

    fix: token error

--- middlewares/authVerify.middleware.js
+++ middlewares/authVerify.middleware.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
 const mySecret = process.env['TOKEN_KEY'];
 const authVerify = (req, res, next) => {
-  // const token = req.headers.authorization;
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOiI2MGVhNjJjMWVkNDY2NjAwMjM0MmMxY2UiLCJpYXQiOjE2MjU5NzY0MjQsImV4cCI6MTYyNjA2MjgyNH0.shKq9UiERmVcln2Orx-fXgWSwsxGgiDk_olnVsVsLoU";
+  const token = req.headers.authorization;
   try {
     let decoded = jwt.verify(token, mySecret);
     req.user = {userid:decoded.userid};
